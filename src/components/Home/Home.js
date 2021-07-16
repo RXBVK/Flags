8bf2f49ca516795512e1068482d0916f202d7911
RXBVK <52883785+RXBVK@users.noreply.github.com>
2021-07-16 11:07:46 +0200

    UI improvements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Divider from "../../utils/Divider";
 import { useHistory } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   let history = useHistory();
 
   return (
-    <div className="container">
+    <div className="container home__container">
       <h1>Flags</h1>
       <Divider height={3} width={20} />
       <h2>Test and improve your flags recognition skills!</h2>
-      <p>
+      <p className="home__precise-description">
         In this game you will be able to educate and/or test yourself about
         flags of countries.
       </p>
-      <h3>Choose the game!</h3>
-      <div>
+      <div className="home__game-choice">
+        <h3>Choose the game!</h3>
         <button
           className="button button--basic"
           onClick={() => history.push("/country-to-flag")}
